b711d736dd148191efc3061621dc5553a6672cab
doongjun <doongjun@kakao.com>
2023-06-02 23:36:23 +0900

    initial commit

---
+++ src/main/kotlin/com/liah/doribottle/DoriBottleApplication.kt
@@ -0,0 +1,11 @@
+package com.liah.doribottle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DoriBottleApplication
+
+fun main(args: Array<String>) {
+	runApplication<DoriBottleApplication>(*args)
+}
---
+++ src/test/kotlin/com/liah/doribottle/DoriBottleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.liah.doribottle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DoriBottleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
