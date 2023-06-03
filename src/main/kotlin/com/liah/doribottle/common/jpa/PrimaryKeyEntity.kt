91237bb9ca2c692adcc1ba70a9a3dc271ab3682e
doongjun <doongjun.kim@gmail.com>
2023-06-03 16:42:36 +0900

    feat: Add PrimaryKey base entity

---
+++ src/main/kotlin/com/liah/doribottle/common/jpa/PrimaryKeyEntity.kt
@@ -0,0 +1,62 @@
+package com.liah.doribottle.common.jpa
+
+import com.github.f4b6a3.ulid.UlidCreator
+import jakarta.persistence.*
+import org.hibernate.proxy.HibernateProxy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.domain.Persistable
+import java.io.Serializable
+import java.time.Instant
+import java.util.*
+import kotlin.jvm.Transient
+
+@MappedSuperclass
+abstract class PrimaryKeyEntity : Persistable<UUID> {
+    @Id
+    @Column(columnDefinition = "uuid")
+    private val id: UUID = UlidCreator.getMonotonicUlid().toUuid()
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    val createdDate: Instant = Instant.now()
+
+    @LastModifiedDate
+    @Column(nullable = false, updatable = false)
+    val lastModifiedDate: Instant = Instant.now()
+
+    @Transient
+    private var _isNew = true
+
+    override fun getId() = id
+
+    override fun isNew() = _isNew
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) {
+            return false
+        }
+
+        if (other !is HibernateProxy && this::class != other::class) {
+            return false
+        }
+
+        return id == getIdentifier(other)
+    }
+
+    private fun getIdentifier(obj: Any): Serializable {
+        return if (obj is HibernateProxy) {
+            obj.hibernateLazyInitializer.identifier as Serializable
+        } else {
+            (obj as PrimaryKeyEntity).id
+        }
+    }
+
+    override fun hashCode() = Objects.hashCode(id)
+
+    @PostPersist
+    @PostLoad
+    protected fun load() {
+        _isNew = false
+    }
+}
