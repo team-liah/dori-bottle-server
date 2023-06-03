aa6b9fa2094b79583dd31c2ffa6b9e35c4c5e46d
doongjun <doongjun@kakao.com>
2023-06-03 17:47:50 +0900

    fix: Add Transactional Annotation for each independent test

--- src/test/kotlin/com/liah/doribottle/user/UserServiceTest.kt
+++ src/test/kotlin/com/liah/doribottle/user/UserServiceTest.kt
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
+@Transactional
 class UserServiceTest {
     companion object {
         const val LOGIN_ID = "010-0000-0000"
