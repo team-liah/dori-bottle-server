685dd37ad9bbbaf64fe8da05468e9181e42039bf
doongjun <doongjun@kakao.com>
2023-06-03 21:51:14 +0900

    feat: Add custom exception

---
+++ src/main/kotlin/com/liah/doribottle/common/exception/ForbiddenException.kt
@@ -0,0 +1,3 @@
+package com.liah.doribottle.common.exception
+
+class ForbiddenException(message: String) : RuntimeException(message)
---
+++ src/main/kotlin/com/liah/doribottle/common/exception/NotFoundException.kt
@@ -0,0 +1,3 @@
+package com.liah.doribottle.common.exception
+
+class NotFoundException(message: String) : RuntimeException(message)
