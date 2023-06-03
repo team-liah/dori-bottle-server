9c6648347c8b763fffbb4d3eb40e3146c6a60ba4
doongjun <doongjun@kakao.com>
2023-06-03 17:48:32 +0900

    chore: configure Spring Security

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.security:spring-security-test")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 
