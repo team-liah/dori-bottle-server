03a0567c5994c4748f5c64022766e3862b643bbe
doongjun <doongjun.kim@gmail.com>
2023-06-05 16:48:08 +0900

    rename: Change package structure

--- src/test/kotlin/com/liah/doribottle/service/UserServiceTest.kt
+++ src/test/kotlin/com/liah/doribottle/service/UserServiceTest.kt
@@ -1,9 +1,8 @@
-package com.liah.doribottle.user
+package com.liah.doribottle.service
 
 import com.liah.doribottle.domain.user.User
 import com.liah.doribottle.domain.user.UserRepository
 import com.liah.doribottle.domain.user.UserRole.ROLE_USER
-import com.liah.doribottle.service.UserService
 import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
 import org.assertj.core.api.Assertions.assertThat
