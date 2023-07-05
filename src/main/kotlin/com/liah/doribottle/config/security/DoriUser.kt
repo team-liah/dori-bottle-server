ca20fa7ec8eef36348cf63e552fc74fd1b485c98
doongjun <doongjun@kakao.com>
2023-07-05 19:06:49 +0900

    feat: Add role at DoriUser

--- src/main/kotlin/com/liah/doribottle/config/security/DoriUser.kt
+++ src/main/kotlin/com/liah/doribottle/config/security/DoriUser.kt
@@ -1,8 +1,10 @@
 package com.liah.doribottle.config.security
 
+import com.liah.doribottle.domain.user.Role
 import java.util.*
 
 data class DoriUser(
     val id: UUID,
-    val login: String
+    val login: String,
+    val role: Role
 )
