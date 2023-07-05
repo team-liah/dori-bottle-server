3d4e88d01d4e55eedf58944b0b5d3be2323c5ff0
doongjun <doongjun@kakao.com>
2023-07-05 10:15:37 +0900

    feat: Add error log

--- src/main/kotlin/com/liah/doribottle/service/sms/ToastApiClient.kt
+++ src/main/kotlin/com/liah/doribottle/service/sms/ToastApiClient.kt
@@ -5,6 +5,7 @@ import com.liah.doribottle.service.sms.dto.Recipient
 import com.liah.doribottle.service.sms.dto.SendAuthSmsTemplateRequest
 import com.liah.doribottle.service.sms.dto.ToastRestApiResponse
 import com.liah.doribottle.service.sms.dto.ToastTemplate
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -17,6 +18,7 @@ class ToastApiClient(
     @Value("\${toast.sms.secretKey}") private val smsSecretKey: String,
     @Value("\${toast.sms.sendNo}") private val sendNo: String
 ) {
+    private val log = LoggerFactory.getLogger(javaClass)
     private val authSmsSendRequestUrl = "${smsUrl}/sms/v3.0/appKeys/${smsAppKey}/sender/auth/sms"
 
     fun sendAuthSmsTemplate(
@@ -34,7 +36,10 @@ class ToastApiClient(
         val response = RestTemplate()
             .postForEntity(authSmsSendRequestUrl, request, ToastRestApiResponse::class.java)
 
-        if (response.statusCode != HttpStatus.OK || response.body?.header?.isSuccessful == false)
+        if (response.statusCode != HttpStatus.OK || response.body?.header?.isSuccessful == false) {
+            log.error("ERROR CODE: ${response.body?.header?.resultCode}")
+            log.error("ERROR MESSAGE: ${response.body?.header?.resultMessage}")
             throw SmsSendingException("SMS 발송 실패했습니다.")
+        }
     }
 }
