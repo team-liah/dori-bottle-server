b87c056710c941b2d271b353dd725822ef4f3a3f
doongjun <doongjun@kakao.com>
2023-07-07 00:43:56 +0900

    temp commit

--- src/main/kotlin/com/liah/doribottle/common/exception/SmsSendingException.kt
+++ src/main/kotlin/com/liah/doribottle/common/exception/SmsSendingException.kt
@@ -1,3 +1,3 @@
 package com.liah.doribottle.common.exception
 
-class SmsSendingException(message: String? = "SMS 발송 실패했습니다.") : RuntimeException(message)
+class SmsSendingException(errorCode: ErrorCode) : BusinessException(errorCode)
