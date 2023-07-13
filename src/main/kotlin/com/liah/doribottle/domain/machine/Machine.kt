9ffee01e232b57d18d29bd11612d2b239d2b2586
doongjun <doongjun.kim@gmail.com>
2023-07-13 13:22:29 +0900

    feat: Machine Service

--- src/main/kotlin/com/liah/doribottle/domain/machine/Machine.kt
+++ src/main/kotlin/com/liah/doribottle/domain/machine/Machine.kt
@@ -2,9 +2,9 @@ package com.liah.doribottle.domain.machine
 
 import com.liah.doribottle.domain.common.Address
 import com.liah.doribottle.domain.common.PrimaryKeyEntity
-import com.liah.doribottle.domain.cup.Cup
-import com.liah.doribottle.domain.cup.CupStatus
+import com.liah.doribottle.domain.machine.MachineState.INITIAL
 import com.liah.doribottle.domain.machine.MachineType.*
+import com.liah.doribottle.service.machine.dto.MachineDto
 import jakarta.persistence.*
 
 @Entity
@@ -16,8 +16,7 @@ class Machine(
     no: String,
     type: MachineType,
     address: Address,
-    capacity: Long,
-    state: MachineState
+    capacity: Long
 ) : PrimaryKeyEntity() {
     @Column(nullable = false, unique = true)
     val no: String = no
@@ -26,7 +25,7 @@ class Machine(
     @Column(nullable = false)
     val type: MachineType = type
 
-    @Column(nullable = false)
+    @Embedded
     var address: Address = address
         protected set
 
@@ -35,26 +34,13 @@ class Machine(
         protected set
 
     @Column(nullable = false)
-    var state: MachineState = state
+    var cupAmounts: Long = 0
         protected set
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "machine")
-    protected val mutableCups: MutableSet<Cup> = mutableSetOf()
-    val cups: Set<Cup> get() = mutableCups
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var state: MachineState = INITIAL
+        protected set
 
-    fun addCup(cup: Cup) {
-        val possible = when (type) {
-            VENDING -> (cup.status == CupStatus.WASHING || cup.status == CupStatus.PENDING)
-            COLLECTION -> cup.status == CupStatus.ON_LOAN
-            WASHING -> cup.status == CupStatus.PENDING
-        }
-
-        mutableCups.add(cup)
-        cup.toMachine(this)
-    }
-
-    fun removeCup(cup: Cup) {
-        mutableCups.remove(cup)
-        cup.toMachine(null)
-    }
+    fun toDto() = MachineDto(id, no, type, address, capacity, cupAmounts, state)
 }
