1969dc1af35eab33dfe3b0a62686c4e1ca22658e
doongjun <doongjun.kim@gmail.com>
2023-07-17 13:08:54 +0900

    feat: Update machine

--- src/main/kotlin/com/liah/doribottle/domain/machine/Machine.kt
+++ src/main/kotlin/com/liah/doribottle/domain/machine/Machine.kt
@@ -3,23 +3,27 @@ package com.liah.doribottle.domain.machine
 import com.liah.doribottle.common.error.exception.BusinessException
 import com.liah.doribottle.common.error.exception.ErrorCode
 import com.liah.doribottle.domain.common.Address
-import com.liah.doribottle.domain.common.PrimaryKeyEntity
+import com.liah.doribottle.domain.common.SoftDeleteEntity
 import com.liah.doribottle.domain.machine.MachineState.INITIAL
 import com.liah.doribottle.domain.machine.MachineType.*
 import com.liah.doribottle.service.machine.dto.MachineDto
 import jakarta.persistence.*
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
 
 @Entity
 @Table(
     name = "machine",
     indexes = [Index(name = "IDX_MACHINE_NO", columnList = "no")]
 )
+@SQLDelete(sql = "UPDATE machine SET deleted = true where id = ?")
+@Where(clause = "deleted = false")
 class Machine(
     no: String,
     type: MachineType,
     address: Address,
-    capacity: Long
-) : PrimaryKeyEntity() {
+    capacity: Int
+) : SoftDeleteEntity() {
     @Column(nullable = false, unique = true)
     val no: String = no
 
@@ -32,7 +36,7 @@ class Machine(
         protected set
 
     @Column(nullable = false)
-    var capacity: Long = capacity
+    var capacity: Int = capacity
         protected set
 
     @Column(nullable = false)
@@ -44,12 +48,23 @@ class Machine(
     var state: MachineState = INITIAL
         protected set
 
-    fun increaseCupAmounts(amounts: Int) {
-        val result = cupAmounts + amounts
-        if (result > capacity) throw BusinessException(ErrorCode.FULL_OF_CUP)
-        if (result < 0) throw BusinessException(ErrorCode.LACK_OF_CUP)
+    fun update(
+        address: Address,
+        capacity: Int
+    ) {
+        this.address = address
+        this.capacity = capacity
+    }
 
-        cupAmounts = result
+    fun increaseCupAmounts(amounts: Int) {
+        updateCupAmounts(cupAmounts + amounts)
+    }
+
+    fun updateCupAmounts(amounts: Int) {
+        if (amounts > capacity) throw BusinessException(ErrorCode.FULL_OF_CUP)
+        if (amounts < 0) throw BusinessException(ErrorCode.LACK_OF_CUP)
+
+        cupAmounts = amounts
     }
 
     fun toDto() = MachineDto(id, no, type, address, capacity, cupAmounts, state)
