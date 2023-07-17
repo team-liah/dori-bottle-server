9b7b0271022d83bb3a2bef1c820bc15974e858d5
doongjun <doongjun@kakao.com>
2023-07-17 13:09:12 +0900

    test: Update machin e

--- src/test/kotlin/com/liah/doribottle/service/machine/MachineServiceTest.kt
+++ src/test/kotlin/com/liah/doribottle/service/machine/MachineServiceTest.kt
@@ -2,9 +2,9 @@ package com.liah.doribottle.service.machine
 
 import com.liah.doribottle.domain.common.Address
 import com.liah.doribottle.domain.machine.Machine
-import com.liah.doribottle.repository.machine.MachineRepository
 import com.liah.doribottle.domain.machine.MachineState.INITIAL
 import com.liah.doribottle.domain.machine.MachineType
+import com.liah.doribottle.repository.machine.MachineRepository
 import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
 import org.assertj.core.api.Assertions.assertThat
@@ -32,7 +32,7 @@ class MachineServiceTest {
 
     @DisplayName("자판기 등록")
     @Test
-    fun registerTest() {
+    fun register() {
         //given
         val no = "0000000"
         val address = Address("12345", "삼성로", null)
@@ -53,7 +53,7 @@ class MachineServiceTest {
 
     @DisplayName("자판기 조회")
     @Test
-    fun getTest() {
+    fun get() {
         //given
         val no = "0000000"
         val address = Address("12345", "삼성로", null)
@@ -70,5 +70,31 @@ class MachineServiceTest {
         assertThat(machineDto.capacity).isEqualTo(100)
         assertThat(machineDto.cupAmounts).isEqualTo(0)
         assertThat(machineDto.state).isEqualTo(INITIAL)
+    }
+
+    @DisplayName("자판기 정보 수정")
+    @Test
+    fun update() {
+        //given
+        val no = "0000000"
+        val address = Address("12345", "삼성로", null)
+        val machine = machineRepository.save(Machine(no, MachineType.VENDING, address, 100))
+        clear()
+
+        //when
+        val newAddress = Address("00000", "마장로", null)
+        machineService.update(machine.id, newAddress, 200, 10)
+        clear()
+
+        //then
+        val findMachine = machineRepository.findByIdOrNull(machine.id)
+        assertThat(findMachine?.no).isEqualTo(no)
+        assertThat(findMachine?.type).isEqualTo(MachineType.VENDING)
+        assertThat(findMachine?.address?.zipCode).isEqualTo("00000")
+        assertThat(findMachine?.address?.address1).isEqualTo("마장로")
+        assertThat(findMachine?.address?.address2).isNull()
+        assertThat(findMachine?.capacity).isEqualTo(200)
+        assertThat(findMachine?.cupAmounts).isEqualTo(10)
+        assertThat(findMachine?.state).isEqualTo(INITIAL)
     }
 }
