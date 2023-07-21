5b989a5e0833d3f82b028c4a53d24c65783d8ef0
doongjun <doongjun@kakao.com>
2023-07-21 18:49:03 +0900

    refactor: Add BaseControllerTest

--- src/test/kotlin/com/liah/doribottle/web/v1/account/AccountControllerTest.kt
+++ src/test/kotlin/com/liah/doribottle/web/v1/account/AccountControllerTest.kt
@@ -1,7 +1,6 @@
 package com.liah.doribottle.web.v1.account
 
 import com.liah.doribottle.common.error.exception.ErrorCode
-import com.liah.doribottle.config.security.TokenProvider
 import com.liah.doribottle.config.security.WithMockDoriUser
 import com.liah.doribottle.constant.ACCESS_TOKEN
 import com.liah.doribottle.constant.REFRESH_TOKEN
@@ -10,6 +9,7 @@ import com.liah.doribottle.domain.user.Gender.MALE
 import com.liah.doribottle.extension.convertJsonToString
 import com.liah.doribottle.repository.user.RefreshTokenRepository
 import com.liah.doribottle.repository.user.UserRepository
+import com.liah.doribottle.web.BaseControllerTest
 import com.liah.doribottle.web.v1.account.vm.AuthRequest
 import com.liah.doribottle.web.v1.account.vm.RegisterRequest
 import com.liah.doribottle.web.v1.account.vm.SendSmsRequest
@@ -19,38 +19,18 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
-import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 import java.util.*
 
-@ExtendWith(SpringExtension::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class AccountControllerTest {
-    private lateinit var mockMvc: MockMvc
+class AccountControllerTest : BaseControllerTest() {
     private val endPoint = "/api/v1/account"
-
-    companion object {
-        private const val USER_LOGIN_ID = "010-5638-3316"
-        private const val GUEST_LOGIN_ID = "010-1234-5678"
-    }
-    @Autowired private lateinit var context: WebApplicationContext
 
     @Autowired private lateinit var userRepository: UserRepository
     @Autowired private lateinit var refreshTokenRepository: RefreshTokenRepository
-    @Autowired private lateinit var passwordEncoder: PasswordEncoder
-    @Autowired private lateinit var tokenProvider: TokenProvider
 
     private lateinit var user: User
     private lateinit var guest: User
@@ -59,17 +39,9 @@ class AccountControllerTest {
     private lateinit var guestRefreshToken: RefreshToken
 
     @BeforeEach
-    internal fun setUp() {
-        mockMvc = MockMvcBuilders
-            .webAppContextSetup(context)
-            .apply<DefaultMockMvcBuilder?>(springSecurity())
-            .build()
-    }
-
-    @BeforeEach
     internal fun init() {
         val userEntity = User(USER_LOGIN_ID, "Tester 1", USER_LOGIN_ID, Role.USER)
-        userEntity.updatePassword(passwordEncoder.encode("123456"))
+        userEntity.updatePassword(encodePassword("123456"))
         user = userRepository.save(userEntity)
         userRefreshToken = refreshTokenRepository.save(RefreshToken(user))
 
@@ -165,8 +137,7 @@ class AccountControllerTest {
     @DisplayName("Dori User Pre Auth Token")
     @Test
     fun getPreAuthToken() {
-        val accessToken = tokenProvider.createToken(user.id, user.loginId, user.role)
-        val cookie = Cookie(ACCESS_TOKEN, accessToken)
+        val cookie = createAccessTokenCookie(user.id, user.loginId, user.role)
 
         mockMvc.perform(
             MockMvcRequestBuilders.get("$endPoint/pre-auth")
--- src/test/kotlin/com/liah/doribottle/web/v1/point/PointControllerTest.kt
+++ src/test/kotlin/com/liah/doribottle/web/v1/point/PointControllerTest.kt
@@ -1,59 +1,30 @@
 package com.liah.doribottle.web.v1.point
 
-import com.liah.doribottle.config.security.TokenProvider
-import com.liah.doribottle.constant.ACCESS_TOKEN
 import com.liah.doribottle.domain.point.Point
 import com.liah.doribottle.domain.point.PointEventType
 import com.liah.doribottle.domain.point.PointSaveType
 import com.liah.doribottle.domain.user.*
 import com.liah.doribottle.repository.point.PointRepository
 import com.liah.doribottle.repository.user.UserRepository
-import jakarta.servlet.http.Cookie
+import com.liah.doribottle.web.BaseControllerTest
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
-import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 
-@ExtendWith(SpringExtension::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class PointControllerTest {
-    private lateinit var mockMvc: MockMvc
+class PointControllerTest : BaseControllerTest() {
     private val endPoint = "/api/v1/point"
-
-    companion object {
-        private const val USER_LOGIN_ID = "010-5638-3316"
-    }
-
-    @Autowired private lateinit var context: WebApplicationContext
 
     @Autowired private lateinit var userRepository: UserRepository
     @Autowired private lateinit var pointRepository: PointRepository
 
-    @Autowired private lateinit var tokenProvider: TokenProvider
-
     private lateinit var user: User
-
-    @BeforeEach
-    internal fun setUp() {
-        mockMvc = MockMvcBuilders
-            .webAppContextSetup(context)
-            .apply<DefaultMockMvcBuilder?>(SecurityMockMvcConfigurers.springSecurity())
-            .build()
-    }
 
     @BeforeEach
     internal fun init() {
@@ -73,8 +44,7 @@ class PointControllerTest {
         pointRepository.save(Point(user.id, PointSaveType.REWARD, PointEventType.SAVE_REGISTER_REWARD, 10))
         pointRepository.save(Point(user.id, PointSaveType.PAY, PointEventType.SAVE_PAY, 10))
 
-        val accessToken = tokenProvider.createToken(user.id, user.loginId, user.role)
-        val cookie = Cookie(ACCESS_TOKEN, accessToken)
+        val cookie = createAccessTokenCookie(user.id, user.loginId, user.role)
 
         mockMvc.perform(
             MockMvcRequestBuilders.get("$endPoint/remain-point")
