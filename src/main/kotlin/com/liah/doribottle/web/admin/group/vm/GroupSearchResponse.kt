f9a9861425eb881b4137f98d93fcba616ace5f88
doongjun <doongjun@kakao.com>
2023-07-25 10:42:33 +0900

    feat: Group api

---
+++ src/main/kotlin/com/liah/doribottle/web/admin/group/vm/GroupSearchResponse.kt
@@ -0,0 +1,10 @@
+package com.liah.doribottle.web.admin.group.vm
+
+import com.liah.doribottle.domain.group.GroupType
+import java.util.UUID
+
+data class GroupSearchResponse(
+    val id: UUID,
+    val name: String,
+    val type: GroupType
+)
