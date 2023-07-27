49129e38ac7c64e67fad7fface89a79fc47ca345
doongjun <doongjun.kim@gmail.com>
2023-07-27 16:00:35 +0900

    feat: Get Payment categories api

--- src/main/kotlin/com/liah/doribottle/service/payment/dto/PaymentCategoryDto.kt
+++ src/main/kotlin/com/liah/doribottle/service/payment/dto/PaymentCategoryDto.kt
@@ -1,14 +1,17 @@
 package com.liah.doribottle.service.payment.dto
 
+import com.liah.doribottle.web.v1.payment.vm.PaymentCategorySearchResponse
 import java.time.Instant
-import java.util.UUID
+import java.util.*
 
 data class PaymentCategoryDto(
     val id: UUID,
     val amounts: Long,
     val price: Long,
-    val discountRate: Int?,
+    val discountRate: Int,
     val discountExpiredDate: Instant?,
     val expiredDate: Instant?,
     val deleted: Boolean
-)
+) {
+    fun toUserResponse() = PaymentCategorySearchResponse(id, amounts, price, discountRate, price-(price*discountRate/100), discountExpiredDate, expiredDate)
+}
