ed5bed13cf762d7876c4acdb9b970d6b43cba2e4
doongjun <doongjun@kakao.com>
2023-07-28 16:32:11 +0900

    feat: Notification Api

--- src/main/kotlin/com/liah/doribottle/service/notification/dto/NotificationDto.kt
+++ src/main/kotlin/com/liah/doribottle/service/notification/dto/NotificationDto.kt
@@ -1,6 +1,7 @@
 package com.liah.doribottle.service.notification.dto
 
 import com.liah.doribottle.domain.notification.NotificationType
+import com.liah.doribottle.web.v1.notification.vm.NotificationSearchResponse
 import java.util.*
 
 data class NotificationDto(
@@ -11,4 +12,6 @@ data class NotificationDto(
     val content: String,
     val targetId: UUID?,
     val read: Boolean
-)
+) {
+    fun toSearchResponse() = NotificationSearchResponse(id, userId, type, title, content, targetId, read)
+}
---
+++ src/main/kotlin/com/liah/doribottle/web/v1/notification/NotificationController.kt
@@ -0,0 +1,37 @@
+package com.liah.doribottle.web.v1.notification
+
+import com.liah.doribottle.common.pageable.CustomPage
+import com.liah.doribottle.extension.currentUserId
+import com.liah.doribottle.service.notification.NotificationService
+import com.liah.doribottle.web.v1.notification.vm.NotificationSearchResponse
+import org.springdoc.core.annotations.ParameterObject
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort.Direction.DESC
+import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/api/v1/notification")
+class NotificationController(
+    private val notificationService: NotificationService
+) {
+    @GetMapping
+    fun getAll(
+        @ParameterObject @PageableDefault(sort = ["createdDate"], direction = DESC) pageable: Pageable
+    ): CustomPage<NotificationSearchResponse> {
+        val result = notificationService.getAll(
+            userId = currentUserId()!!,
+            pageable = pageable
+        ).map { it.toSearchResponse() }
+
+        return CustomPage.of(result)
+    }
+
+    @PatchMapping("/{id}/read")
+    fun read(
+        @PathVariable id: UUID
+    ) {
+        notificationService.read(id)
+    }
+}
