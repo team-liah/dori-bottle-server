2e7ce9451ff28dd19024461584e107a2fa6f4bb1
doongjun <doongjun.kim@gmail.com>
2023-08-08 00:27:47 +0900

    chore: Caching use Redis

--- src/main/kotlin/com/liah/doribottle/config/RedisConfig.kt
+++ src/main/kotlin/com/liah/doribottle/config/RedisConfig.kt
@@ -3,6 +3,7 @@ package com.liah.doribottle.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
@@ -13,5 +14,5 @@ class RedisConfig(
     @Value("\${spring.data.redis.port}") private val port: Int
 ) {
     @Bean
-    fun redisConnectionFactory() = LettuceConnectionFactory(host, port)
+    fun redisConnectionFactory() = LettuceConnectionFactory(RedisStandaloneConfiguration(host, port))
 }
