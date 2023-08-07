c52033e14770e6fc591a52da74e3898445760fce
doongjun <doongjun@kakao.com>
2023-08-08 00:27:47 +0900

    chore: Caching use Redis

---
+++ src/main/kotlin/com/liah/doribottle/config/CacheConfig.kt
@@ -0,0 +1,33 @@
+package com.liah.doribottle.config
+
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+
+@EnableCaching
+@Configuration
+class CacheConfig {
+    @Bean
+    fun redisCacheManagerBuilderCustomizer(): RedisCacheManagerBuilderCustomizer {
+        return RedisCacheManagerBuilderCustomizer { builder: RedisCacheManagerBuilder ->
+            builder
+                .withCacheConfiguration(
+                    "pointSum",
+                    RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeKeysWith(
+                            RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer())
+                        )
+                        .serializeValuesWith(
+                            RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer())
+                        )
+                )
+        }
+    }
+}
