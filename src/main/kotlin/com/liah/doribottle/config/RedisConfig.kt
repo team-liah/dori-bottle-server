ae9edad30fd3c74e7599e3433e090f550a277479
doongjun <doongjun.kim@gmail.com>
2023-08-09 14:00:48 +0900

    chore: Add RedisRepository Classes

--- src/main/kotlin/com/liah/doribottle/config/RedisConfig.kt
+++ src/main/kotlin/com/liah/doribottle/config/RedisConfig.kt
@@ -1,5 +1,7 @@
 package com.liah.doribottle.config
 
+import com.liah.doribottle.config.security.RefreshTokenRepository
+import com.liah.doribottle.repository.notification.AlertRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -7,7 +9,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
-@EnableRedisRepositories
+@EnableRedisRepositories(basePackageClasses = [RefreshTokenRepository::class, AlertRepository::class])
 @Configuration
 class RedisConfig(
     @Value("\${spring.data.redis.host}") private val host: String,
