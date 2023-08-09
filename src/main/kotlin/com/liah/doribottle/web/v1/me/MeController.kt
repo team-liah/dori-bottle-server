85b180429e2625dafa162a5b7b1aff53abe4c147
doongjun <doongjun.kim@gmail.com>
2023-08-09 14:02:54 +0900

    feat: Add Alert API

--- src/main/kotlin/com/liah/doribottle/web/v1/me/MeController.kt
+++ src/main/kotlin/com/liah/doribottle/web/v1/me/MeController.kt
@@ -2,7 +2,9 @@ package com.liah.doribottle.web.v1.me
 
 import com.liah.doribottle.extension.currentUser
 import com.liah.doribottle.extension.currentUserId
+import com.liah.doribottle.service.notification.NotificationService
 import com.liah.doribottle.service.user.UserService
+import com.liah.doribottle.web.v1.me.vm.MeResponse
 import com.liah.doribottle.web.v1.me.vm.UpdateProfileRequest
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.*
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/v1/me")
 class MeController(
-    private val userService: UserService
+    private val userService: UserService,
+    private val notificationService: NotificationService
 ) {
     @GetMapping
-    fun get() = currentUser()
+    fun get(): MeResponse {
+        val doriUser = currentUser()!!
+        return MeResponse.of(doriUser, notificationService.getAlertCount(doriUser.id))
+    }
 
     @GetMapping("/profile")
     fun getProfile() = userService.get(currentUserId()!!).toProfileResponse()
