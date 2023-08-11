4c762430ecd47e8428ccc2e53b34767988c207f0
doongjun <doongjun@kakao.com>
2023-08-11 16:06:18 +0900

    chore: Add constants

--- src/main/kotlin/com/liah/doribottle/domain/point/PointEventType.kt
+++ src/main/kotlin/com/liah/doribottle/domain/point/PointEventType.kt
@@ -4,6 +4,7 @@ enum class PointEventType(
     val title: String
 ) {
     SAVE_REGISTER_REWARD("회원가입 보상 적립"),
+    SAVE_REGISTER_INVITER_REWARD("초대코드 등록 보상 적립"),
     SAVE_INVITE_REWARD("초대 보상 적립"),
     SAVE_PAY("충전 적립"),
     CANCEL_SAVE("적립 취소"),
