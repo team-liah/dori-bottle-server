d4f19b1048a47d8e302e2f2e0571a8bd0352b70b
doongjun <doongjun.kim@gmail.com>
2023-08-15 21:15:48 +0900

    chore: spring cloud aws starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.security:spring-security-test")
 
+	// aws
+	implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+
 	// aws sqs
 	implementation("io.awspring.cloud:spring-cloud-aws-dependencies:3.0.1")
 	implementation("io.awspring.cloud:spring-cloud-aws-sqs:3.0.1")
