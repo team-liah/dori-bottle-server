15b9f2ee9f649c457b7803d5308f05498933b513
doongjun <doongjun@kakao.com>
2023-08-15 21:16:05 +0900

    chore: Configure SQS

---
+++ src/main/kotlin/com/liah/doribottle/config/AwsSqsConfig.kt
@@ -0,0 +1,42 @@
+package com.liah.doribottle.config
+
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory
+import io.awspring.cloud.sqs.operations.SqsTemplate
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsCredentials
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.sqs.SqsAsyncClient
+
+
+@Configuration
+class AwsSqsConfig(
+    @Value("\${spring.cloud.aws.credentials.access-key}") private val accessKey: String,
+    @Value("\${spring.cloud.aws.credentials.secret-key}") private val secretKey: String,
+) {
+    @Bean
+    fun sqsAsyncClient(): SqsAsyncClient {
+        return SqsAsyncClient.builder()
+            .credentialsProvider {
+                object : AwsCredentials {
+                    override fun accessKeyId() = accessKey
+                    override fun secretAccessKey() = secretKey
+                }
+            }.region(Region.AP_NORTHEAST_2)
+            .build()
+    }
+
+    @Bean
+    fun defaultSqlListenerContainerFactory(): SqsMessageListenerContainerFactory<Any> {
+        return SqsMessageListenerContainerFactory
+            .builder<Any>()
+            .sqsAsyncClient(sqsAsyncClient())
+            .build()
+    }
+
+    @Bean
+    fun sqsTemplate(): SqsTemplate {
+        return SqsTemplate.newTemplate(sqsAsyncClient())
+    }
+}
