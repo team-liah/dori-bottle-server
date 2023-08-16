a65d83c074bdc7e44a5b33cf365bf766742c21b0
doongjun <doongjun.kim@gmail.com>
2023-08-16 10:55:22 +0900

    refactor: applcation.yml

--- src/main/kotlin/com/liah/doribottle/config/AwsSqsConfig.kt
+++ src/main/kotlin/com/liah/doribottle/config/AwsSqsConfig.kt
@@ -5,7 +5,8 @@ import io.awspring.cloud.sqs.operations.SqsTemplate
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import software.amazon.awssdk.auth.credentials.AwsCredentials
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 
@@ -18,12 +19,8 @@ class AwsSqsConfig(
     @Bean
     fun sqsAsyncClient(): SqsAsyncClient {
         return SqsAsyncClient.builder()
-            .credentialsProvider {
-                object : AwsCredentials {
-                    override fun accessKeyId() = accessKey
-                    override fun secretAccessKey() = secretKey
-                }
-            }.region(Region.AP_NORTHEAST_2)
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+            .region(Region.AP_NORTHEAST_2)
             .build()
     }
 
