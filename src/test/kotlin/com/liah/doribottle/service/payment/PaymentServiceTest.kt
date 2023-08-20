8d9b7bca4540f38e97b366f5aeb2e6dd91430666
doongjun <doongjun.kim@gmail.com>
2023-08-20 10:53:48 +0900

    test: Register PaymentMethod

--- src/test/kotlin/com/liah/doribottle/service/payment/PaymentServiceTest.kt
+++ src/test/kotlin/com/liah/doribottle/service/payment/PaymentServiceTest.kt
@@ -1,8 +1,22 @@
 package com.liah.doribottle.service.payment
 
 import com.liah.doribottle.domain.payment.PaymentCategory
+import com.liah.doribottle.domain.payment.PaymentMethod
+import com.liah.doribottle.domain.payment.PaymentMethodProviderType.TOSS_PAYMENTS
+import com.liah.doribottle.domain.payment.PaymentMethodType.CARD
+import com.liah.doribottle.domain.payment.card.Card
+import com.liah.doribottle.domain.payment.card.CardOwnerType.PERSONAL
+import com.liah.doribottle.domain.payment.card.CardProvider.HYUNDAI
+import com.liah.doribottle.domain.payment.card.CardProvider.KOOKMIN
+import com.liah.doribottle.domain.payment.card.CardType.CREDIT
+import com.liah.doribottle.domain.user.Role
+import com.liah.doribottle.domain.user.User
 import com.liah.doribottle.repository.payment.PaymentCategoryRepository
+import com.liah.doribottle.repository.payment.PaymentMethodRepository
+import com.liah.doribottle.repository.user.UserRepository
 import com.liah.doribottle.service.BaseServiceTest
+import com.liah.doribottle.service.payment.dto.BillingInfo
+import com.liah.doribottle.service.payment.dto.CardDto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -16,7 +30,66 @@ class PaymentServiceTest : BaseServiceTest() {
     @Autowired
     private lateinit var paymentService: PaymentService
     @Autowired
+    private lateinit var paymentMethodRepository: PaymentMethodRepository
+    @Autowired
     private lateinit var paymentCategoryRepository: PaymentCategoryRepository
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @DisplayName("결제 수단 등록")
+    @Test
+    fun registerMethod() {
+        //given
+        val user = userRepository.save(User(USER_LOGIN_ID, "Tester", USER_LOGIN_ID, Role.USER))
+        val billingInfo = BillingInfo("dummyKey", TOSS_PAYMENTS, CARD, CardDto(HYUNDAI, HYUNDAI, "1234", CREDIT, PERSONAL), Instant.now())
+        clear()
+
+        //when
+        val id = paymentService.registerMethod(user.id, billingInfo)
+        clear()
+
+        //then
+        val findMethod = paymentMethodRepository.findByIdOrNull(id)
+
+        assertThat(findMethod?.user).isEqualTo(user)
+        assertThat(findMethod?.billingKey).isEqualTo("dummyKey")
+        assertThat(findMethod?.providerType).isEqualTo(TOSS_PAYMENTS)
+        assertThat(findMethod?.type).isEqualTo(CARD)
+        assertThat(findMethod?.default).isEqualTo(true)
+        assertThat(findMethod?.card?.issuerProvider).isEqualTo(HYUNDAI)
+        assertThat(findMethod?.card?.acquirerProvider).isEqualTo(HYUNDAI)
+        assertThat(findMethod?.card?.number).isEqualTo("1234")
+        assertThat(findMethod?.card?.cardType).isEqualTo(CREDIT)
+        assertThat(findMethod?.card?.cardOwnerType).isEqualTo(PERSONAL)
+    }
+
+    @DisplayName("결제 수단 등록 TC2")
+    @Test
+    fun registerMethodTc2() {
+        //given
+        val user = userRepository.save(User(USER_LOGIN_ID, "Tester", USER_LOGIN_ID, Role.USER))
+        paymentMethodRepository.save(PaymentMethod(user, TOSS_PAYMENTS, "dummyKey1", CARD, Card(KOOKMIN, KOOKMIN, "4321", CREDIT, PERSONAL), Instant.now(), true))
+        val billingInfo = BillingInfo("dummyKey2", TOSS_PAYMENTS, CARD, CardDto(HYUNDAI, HYUNDAI, "1234", CREDIT, PERSONAL), Instant.now())
+        clear()
+
+        //when
+        val id = paymentService.registerMethod(user.id, billingInfo)
+        clear()
+
+        //then
+        val findMethod = paymentMethodRepository.findByIdOrNull(id)
+
+        assertThat(findMethod?.user).isEqualTo(user)
+        assertThat(findMethod?.billingKey).isEqualTo("dummyKey2")
+        assertThat(findMethod?.providerType).isEqualTo(TOSS_PAYMENTS)
+        assertThat(findMethod?.type).isEqualTo(CARD)
+        assertThat(findMethod?.default).isEqualTo(false)
+        assertThat(findMethod?.card?.issuerProvider).isEqualTo(HYUNDAI)
+        assertThat(findMethod?.card?.acquirerProvider).isEqualTo(HYUNDAI)
+        assertThat(findMethod?.card?.number).isEqualTo("1234")
+        assertThat(findMethod?.card?.cardType).isEqualTo(CREDIT)
+        assertThat(findMethod?.card?.cardOwnerType).isEqualTo(PERSONAL)
+    }
 
     @DisplayName("결제 카테고리 등록")
     @Test
