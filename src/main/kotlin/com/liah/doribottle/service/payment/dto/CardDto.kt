f578cdf87152642cfeab52c1a64c0371de88e489
doongjun <doongjun@kakao.com>
2023-08-21 13:17:20 +0900

    feat: Register, Search PaymentMethod API

--- src/main/kotlin/com/liah/doribottle/service/payment/dto/CardDto.kt
+++ src/main/kotlin/com/liah/doribottle/service/payment/dto/CardDto.kt
@@ -4,6 +4,7 @@ import com.liah.doribottle.domain.payment.card.Card
 import com.liah.doribottle.domain.payment.card.CardOwnerType
 import com.liah.doribottle.domain.payment.card.CardProvider
 import com.liah.doribottle.domain.payment.card.CardType
+import com.liah.doribottle.web.v1.payment.vm.CardResponse
 
 data class CardDto(
     val issuerProvider: CardProvider,
@@ -13,4 +14,5 @@ data class CardDto(
     val cardOwnerType: CardOwnerType
 ) {
     fun toEmbeddable() = Card(issuerProvider, acquirerProvider, number, cardType, cardOwnerType)
+    fun toResponse() = CardResponse(acquirerProvider.title, number.substring(0 until 4), cardType.title, cardOwnerType.title)
 }
