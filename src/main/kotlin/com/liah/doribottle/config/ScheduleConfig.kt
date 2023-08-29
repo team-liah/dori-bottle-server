00e5ee360d939f0065c2ec785d9ebfcb586b967f
doongjun <doongjun@kakao.com>
2023-08-30 00:07:17 +0900

    chore: Schedule Config

---
+++ src/main/kotlin/com/liah/doribottle/config/ScheduleConfig.kt
@@ -0,0 +1,8 @@
+package com.liah.doribottle.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@EnableScheduling
+@Configuration
+class ScheduleConfig
