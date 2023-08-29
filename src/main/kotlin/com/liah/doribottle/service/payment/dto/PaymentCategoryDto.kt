9d4ce4f132511b4982ed52a9881d70e93ee679e8
doongjun <doongjun@kakao.com>
2023-08-29 10:30:31 +0900

    fix: Add payment category discount user response

--- src/main/kotlin/com/liah/doribottle/service/payment/dto/PaymentCategoryDto.kt
+++ src/main/kotlin/com/liah/doribottle/service/payment/dto/PaymentCategoryDto.kt
@@ -23,6 +23,11 @@ data class PaymentCategoryDto(
             price-(price*discountRate/100)
         }
     }
-    fun toUserResponse() = PaymentCategorySearchResponse(id, amounts, price, discountRate, price-(price*discountRate/100), discountExpiredDate, expiredDate)
+    fun toUserResponse(): PaymentCategorySearchResponse {
+        val now = Instant.now()
+        val discountExpired = discountExpiredDate != null && discountExpiredDate.isBefore(now)
+        val discountRate = if (discountExpired) 0 else this.discountRate
+        return PaymentCategorySearchResponse(id, amounts, price, discountRate, price-(price*discountRate/100), discountExpiredDate, expiredDate)
+    }
     fun toAdminResponse() = com.liah.doribottle.web.admin.payment.vm.PaymentCategorySearchResponse(id, amounts, price, discountRate, price-(price*discountRate/100), discountExpiredDate, expiredDate, deleted)
 }
