3f213c4bb6fbef3841ee0b040ebcd60780553632
doongjun <doongjun.kim@gmail.com>
2023-08-31 18:27:36 +0900

    feat: Block User Service

---
+++ src/main/kotlin/com/liah/doribottle/domain/user/BlockedCauseType.kt
@@ -0,0 +1,12 @@
+package com.liah.doribottle.domain.user
+
+import com.liah.doribottle.constant.FIVE_PENALTIES_PRICE
+import com.liah.doribottle.constant.LOST_CUP_PRICE
+
+enum class BlockedCauseType(
+    description: String,
+    clearPrice: Long
+) {
+    FIVE_PENALTIES("페널티 5개 이상", FIVE_PENALTIES_PRICE),
+    LOST_CUP_PENALTY("분실 컵 페널티", LOST_CUP_PRICE)
+}
