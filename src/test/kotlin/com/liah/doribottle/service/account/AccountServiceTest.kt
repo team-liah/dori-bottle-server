b2e269e10aa33b461608efaad699fbb3358f2105
doongjun <doongjun@kakao.com>
2023-09-05 23:08:18 +0900

    comment: Remove TODO

--- src/test/kotlin/com/liah/doribottle/service/account/AccountServiceTest.kt
+++ src/test/kotlin/com/liah/doribottle/service/account/AccountServiceTest.kt
@@ -34,6 +34,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.authentication.DisabledException
 import org.springframework.security.crypto.password.PasswordEncoder
 import java.time.Instant
 
@@ -120,19 +121,32 @@ class AccountServiceTest : BaseServiceTest() {
     @DisplayName("인증 예외")
     @Test
     fun authException() {
-        //given
-        val saveUser = userRepository.save(User(loginId, "Tester", loginId, Role.USER))
-        val loginPassword = "123456"
-        val encryptedPassword = passwordEncoder.encode(loginPassword)
-        saveUser.updatePassword(encryptedPassword)
-        clear()
-
-        // TODO: DisabledException, LockedException
         //when, then
         val badCredentialsException = assertThrows<BadCredentialsException> {
+            //given
+            val user = User(loginId, "Tester", loginId, Role.USER)
+            val loginPassword = "123456"
+            val encryptedPassword = passwordEncoder.encode(loginPassword)
+            user.updatePassword(encryptedPassword)
+            userRepository.save(user)
+            clear()
+
             accountService.auth(loginId, "000000")
         }
         assertThat(badCredentialsException.message).isEqualTo("Invalid login password.")
+
+        val disabledException = assertThrows<DisabledException> {
+            //given
+            val user = User("010-0001-0001", "Tester", "010-0001-0001", Role.USER)
+            val loginPassword = "123456"
+            val encryptedPassword = passwordEncoder.encode(loginPassword)
+            user.updatePassword(encryptedPassword)
+            user.deactivate()
+            userRepository.save(user)
+
+            accountService.auth("010-0001-0001", "123456")
+        }
+        assertThat(disabledException.message).isEqualTo("Account is disabled.")
     }
 
     @DisplayName("재인증")
