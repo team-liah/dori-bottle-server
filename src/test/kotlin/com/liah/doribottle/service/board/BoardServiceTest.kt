6c21946841cf1300c57407073be1d46257beb1d5
doongjun <doongjun.kim@gmail.com>
2023-09-11 17:34:39 +0900

    test: get, remove board service

--- src/test/kotlin/com/liah/doribottle/service/board/BoardServiceTest.kt
+++ src/test/kotlin/com/liah/doribottle/service/board/BoardServiceTest.kt
@@ -3,6 +3,7 @@ package com.liah.doribottle.service.board
 import com.liah.doribottle.common.error.exception.BusinessException
 import com.liah.doribottle.common.error.exception.ErrorCode
 import com.liah.doribottle.domain.board.Board
+import com.liah.doribottle.domain.board.BoardType.FAQ
 import com.liah.doribottle.domain.board.BoardType.NOTICE
 import com.liah.doribottle.repository.board.BoardRepository
 import com.liah.doribottle.service.BaseServiceTest
@@ -47,5 +48,38 @@ class BoardServiceTest : BaseServiceTest() {
         assertThat(exception.errorCode).isEqualTo(ErrorCode.BOARD_ALREADY_CREATED)
     }
 
+    @DisplayName("게시판 목록 조회")
+    @Test
+    fun getAll() {
+        //given
+        boardRepository.save(Board("공지사항", "공지사항 게시판", NOTICE))
+        boardRepository.save(Board("FAQ", "FAQ", FAQ))
+        clear()
 
+        //when
+        val result = boardService.getAll()
+
+        assertThat(result)
+            .extracting("name")
+            .containsExactly("공지사항", "FAQ")
+        assertThat(result)
+            .extracting("type")
+            .containsExactly(NOTICE, FAQ)
+    }
+
+    @DisplayName("게시판 삭제")
+    @Test
+    fun remove() {
+        //given
+        val board = boardRepository.save(Board("공지사항", "공지사항 게시판", NOTICE))
+        clear()
+
+        //when
+        boardService.remove(board.id)
+        clear()
+
+        //then
+        val findBoard = boardRepository.findByIdOrNull(board.id)
+        assertThat(findBoard).isNull()
+    }
 }
