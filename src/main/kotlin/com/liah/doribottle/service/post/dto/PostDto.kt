8866cc417ab5902b09aecace9518aff2c85391fb
doongjun <doongjun.kim@gmail.com>
2023-09-14 17:25:35 +0900

    feat: Post resource api

--- src/main/kotlin/com/liah/doribottle/service/post/dto/PostDto.kt
+++ src/main/kotlin/com/liah/doribottle/service/post/dto/PostDto.kt
@@ -1,14 +1,16 @@
 package com.liah.doribottle.service.post.dto
 
 import com.liah.doribottle.domain.post.PostType
+import com.liah.doribottle.service.account.dto.AdminDto
+import com.liah.doribottle.web.admin.post.vm.PostDetailSearchResponse
 import com.liah.doribottle.web.v1.post.vm.PostSearchResponse
-import java.util.*
 
 data class PostDto(
-    val authorId: UUID,
+    val author: AdminDto,
     val type: PostType,
     val title: String,
     val content: String
 ) {
     fun toSearchResponse() = PostSearchResponse(type, title, content)
+    fun toDetailSearchResponse() = PostDetailSearchResponse(author, type, title, content)
 }
