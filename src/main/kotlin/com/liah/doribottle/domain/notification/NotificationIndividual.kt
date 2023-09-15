33eb4b32681639a5f64245def3611cb912e68b92
doongjun <doongjun@kakao.com>
2023-09-15 13:06:13 +0900

    feat: Add notification type - auto payment

--- src/main/kotlin/com/liah/doribottle/domain/notification/NotificationIndividual.kt
+++ src/main/kotlin/com/liah/doribottle/domain/notification/NotificationIndividual.kt
@@ -20,6 +20,7 @@ class NotificationIndividual(
             REFUND -> "버블 ${args[0]}개 환불 요청이 처리되었습니다."
             PENALTY -> "'${args[0]}'의 사유로 레드카드가 부여되었습니다."
             LOST_CUP -> "컵의 반납 기한이 초과하여 분실 처리되었습니다. (대여번호: ${args[0]})"
+            AUTO_PAYMENT -> "${args[0]}원 자동결제 되었습니다. (사유: ${args[1]})"
             NEAR_EXPIRATION -> {
                 if (args[0] == "0") {
                     "오늘은 대여하신 컵의 반납일입니다. (대여번호: ${args[1]})"
