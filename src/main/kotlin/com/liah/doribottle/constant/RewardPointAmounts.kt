37aebbea2270b8e453f7ddef5554ba81e3b60f3d
doongjun <doongjun@kakao.com>
2023-09-22 10:42:16 +0900

    fix: SAVE_INVITE_REWARD_AMOUNTS_MAP

--- src/main/kotlin/com/liah/doribottle/constant/RewardPointAmounts.kt
+++ src/main/kotlin/com/liah/doribottle/constant/RewardPointAmounts.kt
@@ -10,6 +10,7 @@ const val SAVE_REGISTER_INVITER_REWARD_AMOUNTS: Long = 10
 val SAVE_INVITE_REWARD_AMOUNTS_MAP = mapOf(
     5 to 5L,
     10 to 10L,
-    25 to 25L,
-    50 to 50L
+    25 to 30L,
+    50 to 60L,
+    100 to 120L
 )
