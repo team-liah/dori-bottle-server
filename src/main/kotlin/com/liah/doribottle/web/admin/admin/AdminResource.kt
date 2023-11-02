4840d928058042a455dc81a8217906554ac754cc
doongjun <doongjun@kakao.com>
2023-11-02 18:45:14 +0900

    feat: Migrate to adminService

---
+++ src/main/kotlin/com/liah/doribottle/web/admin/admin/AdminResource.kt
@@ -0,0 +1,12 @@
+package com.liah.doribottle.web.admin.admin
+
+import com.liah.doribottle.service.account.AdminAccountService
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/admin/api/admin")
+class AdminResource(
+) {
+
+}
