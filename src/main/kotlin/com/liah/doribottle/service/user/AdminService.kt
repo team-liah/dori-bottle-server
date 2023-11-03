ce60677e6c5ff11cd5c853b9d660fd903ae2f4e7
doongjun <doongjun.kim@gmail.com>
2023-11-03 13:04:23 +0900

    feat: Admin crud service

--- src/main/kotlin/com/liah/doribottle/service/user/AdminService.kt
+++ src/main/kotlin/com/liah/doribottle/service/user/AdminService.kt
@@ -5,8 +5,12 @@ import com.liah.doribottle.common.error.exception.ErrorCode
 import com.liah.doribottle.common.error.exception.NotFoundException
 import com.liah.doribottle.domain.user.Admin
 import com.liah.doribottle.domain.user.Role
+import com.liah.doribottle.repository.user.AdminQueryRepository
 import com.liah.doribottle.repository.user.AdminRepository
 import com.liah.doribottle.service.account.dto.AdminDto
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,6 +20,7 @@ import java.util.*
 @Transactional
 class AdminService(
     private val adminRepository: AdminRepository,
+    private val adminQueryRepository: AdminQueryRepository,
     private val passwordEncoder: PasswordEncoder
 ) {
     @Transactional
@@ -46,15 +51,60 @@ class AdminService(
             throw BusinessException(ErrorCode.USER_ALREADY_REGISTERED)
     }
 
-    // TODO: TEST
     @Transactional(readOnly = true)
     fun get(
-        loginId: String
+        id: UUID
     ): AdminDto {
-        val admin = adminRepository.findByLoginId(loginId)
+        val admin = adminRepository.findByIdOrNull(id)
             ?: throw NotFoundException(ErrorCode.USER_NOT_FOUND)
 
         return admin.toDto()
+    }
+
+    @Transactional(readOnly = true)
+    fun getAll(
+        loginId: String? = null,
+        name: String? = null,
+        role: Role? = null,
+        deleted: Boolean? = null,
+        pageable: Pageable
+    ): Page<AdminDto> {
+        return adminQueryRepository.getAll(
+            loginId = loginId,
+            name = name,
+            role = role,
+            deleted = deleted,
+            pageable = pageable
+        ).map { it.toDto() }
+    }
+
+    @Transactional
+    fun update(
+        id: UUID,
+        loginId: String,
+        loginPassword: String,
+        name: String,
+        role: Role
+    ) {
+        val admin = adminRepository.findByIdOrNull(id)
+            ?: throw NotFoundException(ErrorCode.USER_NOT_FOUND)
+
+        admin.update(
+            loginId = loginId,
+            loginPassword = loginPassword,
+            name = name,
+            role = role
+        )
+    }
+
+    @Transactional
+    fun delete(
+        id: UUID
+    ) {
+        val admin = adminRepository.findByIdOrNull(id)
+            ?: throw NotFoundException(ErrorCode.USER_NOT_FOUND)
+
+        admin.delete()
     }
 
     //TODO: Remove
