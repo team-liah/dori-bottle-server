06f9ab406831ba7b3d5a2add281ac8e92f7c0d34
doongjun <doongjun.kim@gmail.com>
2023-11-20 15:17:47 +0900

    feat: Cup resource update, delete

--- src/main/kotlin/com/liah/doribottle/web/admin/cup/CupResource.kt
+++ src/main/kotlin/com/liah/doribottle/web/admin/cup/CupResource.kt
@@ -5,6 +5,7 @@ import com.liah.doribottle.service.cup.CupService
 import com.liah.doribottle.service.cup.dto.CupDto
 import com.liah.doribottle.web.admin.cup.vm.CupRegisterRequest
 import com.liah.doribottle.web.admin.cup.vm.CupSearchRequest
+import com.liah.doribottle.web.admin.cup.vm.CupUpdateRequest
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.validation.Valid
 import org.springdoc.core.annotations.ParameterObject
@@ -37,9 +38,39 @@ class CupResource(
     ): CustomPage<CupDto> {
         val result = cupService.getAll(
             status = request.status,
+            deleted = false,
             pageable = pageable
         )
 
         return CustomPage.of(result)
+    }
+
+    @Operation(summary = "컵 조회")
+    @GetMapping("/{id}")
+    fun get(
+        @PathVariable id: UUID
+    ): CupDto {
+        return cupService.get(id)
+    }
+
+    @Operation(summary = "컵 수정")
+    @PutMapping("/{id}")
+    fun update(
+        @PathVariable id: UUID,
+        @RequestBody request: CupUpdateRequest
+    ) {
+        cupService.update(
+            id = id,
+            rfid = request.rfid!!,
+            status = request.status!!,
+        )
+    }
+
+    @Operation(summary = "컵 목록 조회")
+    @DeleteMapping("/{id}")
+    fun remove(
+        @PathVariable id: UUID
+    ) {
+        cupService.remove(id)
     }
 }
