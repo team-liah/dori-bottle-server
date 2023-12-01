e7c48ffe04e95dad0248f45821c72f1fd14ae6ac
doongjun <doongjun@kakao.com>
2023-12-01 14:51:34 +0900

    feat: Get Rental resource api

--- src/main/kotlin/com/liah/doribottle/web/admin/rental/RentalResource.kt
+++ src/main/kotlin/com/liah/doribottle/web/admin/rental/RentalResource.kt
@@ -1,10 +1,17 @@
 package com.liah.doribottle.web.admin.rental
 
+import com.liah.doribottle.common.pageable.CustomPage
 import com.liah.doribottle.service.rental.RentalService
+import com.liah.doribottle.service.rental.dto.RentalDto
 import com.liah.doribottle.web.admin.rental.vm.RentalCupMapRequest
+import com.liah.doribottle.web.admin.rental.vm.RentalSearchRequest
 import com.liah.doribottle.web.admin.rental.vm.ReturnRequest
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.validation.Valid
+import org.springdoc.core.annotations.ParameterObject
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
@@ -13,6 +20,34 @@ import java.util.*
 class RentalResource(
     private val rentalService: RentalService
 ) {
+    @Operation(summary = "유저 대여 내역 조회")
+    @GetMapping
+    fun getAll(
+        @ParameterObject request: RentalSearchRequest,
+        @ParameterObject @PageableDefault(sort = ["createdDate"], direction = Sort.Direction.DESC) pageable: Pageable
+    ): CustomPage<RentalDto> {
+        val result = rentalService.getAll(
+            no = request.no,
+            userId = request.userId,
+            cupId = request.cupId,
+            fromMachineId = request.fromMachineId,
+            toMachineId = request.toMachineId,
+            status = request.status,
+            expired = request.expired,
+            pageable = pageable
+        )
+
+        return CustomPage.of(result)
+    }
+
+    @Operation(summary = "유저 대여 내역 단건 조회")
+    @GetMapping("/{id}")
+    fun get(
+        @PathVariable id: UUID
+    ): RentalDto {
+        return rentalService.get(id)
+    }
+
     @Operation(summary = "대여 정보 - 컵 매핑")
     @PostMapping("/{id}/map")
     fun mapRentalCup(
