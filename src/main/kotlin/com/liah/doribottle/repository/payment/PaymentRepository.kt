44e426d808d382f832b46537e1e7f395b461c0a1
doongjun <doongjun@kakao.com>
2023-12-04 18:14:13 +0900

    feat: Statistic by date native query

--- src/main/kotlin/com/liah/doribottle/repository/payment/PaymentRepository.kt
+++ src/main/kotlin/com/liah/doribottle/repository/payment/PaymentRepository.kt
@@ -2,8 +2,28 @@ package com.liah.doribottle.repository.payment
 
 import com.liah.doribottle.domain.payment.Payment
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface PaymentRepository : JpaRepository<Payment, UUID>
+interface PaymentRepository : JpaRepository<Payment, UUID> {
+    @Query(value =
+        "select date(convert_tz(p.approved_date, '+00:00', '+09:00')) date, " +
+                "sum(if(p.status = 'SUCCEEDED', p.price, 0)) totalAmount, " +
+                "sum(if(p.status = 'SUCCEEDED' and p.type = 'SAVE_POINT', p.price, 0)) savePointAmount, " +
+                "sum(if(p.status = 'SUCCEEDED' and p.type = 'LOST_CUP', p.price, 0)) lostCupAmount, " +
+                "sum(if(p.status = 'SUCCEEDED' and p.type = 'UNBLOCK_ACCOUNT', p.price, 0)) unblockAccountAmount, " +
+                "sum(if(p.status = 'CANCELED', p.price, 0)) cancelAmount " +
+        "from payment p " +
+        "where convert_tz(p.approved_date, '+00:00', '+09:00') >= str_to_date(:startDate, '%Y%m%d') " +
+        "  and convert_tz(p.approved_date, '+00:00', '+09:00') <= str_to_date(:endDate, '%Y%m%d') " +
+        "group by date",
+        nativeQuery = true
+    )
+    fun findStatisticByApprovedDate(
+        @Param("startDate") startDate: String,
+        @Param("endDate") endDate: String
+    ): List<PaymentStatisticDao>
+}
