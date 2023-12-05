6cb58aa0ea5f207d571c371aef07a34dc6a8a533
doongjun <doongjun@kakao.com>
2023-12-05 17:10:15 +0900

    feat: Add payment resource filter

--- src/main/kotlin/com/liah/doribottle/web/admin/payment/vm/PaymentSearchRequest.kt
+++ src/main/kotlin/com/liah/doribottle/web/admin/payment/vm/PaymentSearchRequest.kt
@@ -2,10 +2,13 @@ package com.liah.doribottle.web.admin.payment.vm
 
 import com.liah.doribottle.domain.payment.PaymentStatus
 import com.liah.doribottle.domain.payment.PaymentType
+import java.time.Instant
 import java.util.*
 
 data class PaymentSearchRequest(
     val userId: UUID?,
     val type: PaymentType?,
-    val status: PaymentStatus?
+    val status: PaymentStatus?,
+    val fromApprovedDate: Instant?,
+    val toApprovedDate: Instant?
 )
