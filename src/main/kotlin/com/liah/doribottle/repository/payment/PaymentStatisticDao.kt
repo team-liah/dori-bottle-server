596e586b7eebbd901a73c361f12e85bbc8621ea1
doongjun <doongjun.kim@gmail.com>
2023-12-05 11:13:01 +0900

    feat: Payment statistic api

--- src/main/kotlin/com/liah/doribottle/repository/payment/PaymentStatisticDao.kt
+++ src/main/kotlin/com/liah/doribottle/repository/payment/PaymentStatisticDao.kt
@@ -2,8 +2,8 @@ package com.liah.doribottle.repository.payment
 
 interface PaymentStatisticDao {
     val date: String?
+    val totalAmount: Long?
     val savePointAmount: Long?
     val lostCupAmount: Long?
     val unblockAccountAmount: Long?
-    val cancelAmount: Long?
 }
