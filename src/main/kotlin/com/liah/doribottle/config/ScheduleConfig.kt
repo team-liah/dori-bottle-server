4f88728db6afb0a3755ab55e14c4e7d8b2f520e3
doongjun <doongjun.kim@gmail.com>
2023-12-07 11:24:26 +0900

    feat: Configure shedlock

--- src/main/kotlin/com/liah/doribottle/config/ScheduleConfig.kt
+++ src/main/kotlin/com/liah/doribottle/config/ScheduleConfig.kt
@@ -1,7 +1,12 @@
 package com.liah.doribottle.config
 
+import net.javacrumbs.shedlock.core.LockProvider
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @ConditionalOnProperty(
@@ -10,5 +15,11 @@ import org.springframework.scheduling.annotation.EnableScheduling
     havingValue = "true"
 )
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT60S")
 @Configuration
-class ScheduleConfig
+class ScheduleConfig {
+    @Bean
+    fun lockProvider(redisConnectionFactory: RedisConnectionFactory): LockProvider {
+        return RedisLockProvider(redisConnectionFactory)
+    }
+}
