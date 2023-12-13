e156a68e8d0c1e7f2c11a8ff8f9ea6770fae2200
doongjun <doongjun@kakao.com>
2023-12-13 18:34:05 +0900

    feat: DoriUser extends security user

--- src/main/kotlin/com/liah/doribottle/config/security/DoriUser.kt
+++ src/main/kotlin/com/liah/doribottle/config/security/DoriUser.kt
@@ -1,6 +1,8 @@
 package com.liah.doribottle.config.security
 
 import com.liah.doribottle.domain.user.Role
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
 import java.util.*
 
 data class DoriUser(
@@ -8,4 +10,4 @@ data class DoriUser(
     val loginId: String,
     val name: String,
     val role: Role
-)
+) : User(id.toString(), "", mutableListOf(SimpleGrantedAuthority(role.key)))
