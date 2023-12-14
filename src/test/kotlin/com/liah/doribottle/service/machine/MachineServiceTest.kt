0fe97f6e5bc7324d26085195b0e4bb95262a5c25
doongjun <doongjun@kakao.com>
2023-12-14 18:23:38 +0900

    test: Machine acl

--- src/test/kotlin/com/liah/doribottle/service/machine/MachineServiceTest.kt
+++ src/test/kotlin/com/liah/doribottle/service/machine/MachineServiceTest.kt
@@ -2,6 +2,8 @@ package com.liah.doribottle.service.machine
 
 import com.liah.doribottle.common.error.exception.BusinessException
 import com.liah.doribottle.common.error.exception.ErrorCode
+import com.liah.doribottle.config.security.WithMockDoriUser
+import com.liah.doribottle.config.security.acl.AclManager
 import com.liah.doribottle.domain.common.Address
 import com.liah.doribottle.domain.common.Location
 import com.liah.doribottle.domain.machine.Machine
@@ -9,6 +11,7 @@ import com.liah.doribottle.domain.machine.MachineState.MALFUNCTION
 import com.liah.doribottle.domain.machine.MachineState.NORMAL
 import com.liah.doribottle.domain.machine.MachineType.COLLECTION
 import com.liah.doribottle.domain.machine.MachineType.VENDING
+import com.liah.doribottle.domain.user.Role
 import com.liah.doribottle.repository.machine.MachineRepository
 import com.liah.doribottle.service.BaseServiceTest
 import com.liah.doribottle.service.common.AddressDto
@@ -20,16 +23,56 @@ import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.acls.domain.BasePermission
+import java.util.*
 
 class MachineServiceTest : BaseServiceTest() {
     @Autowired
     private lateinit var machineRepository: MachineRepository
     @Autowired
     private lateinit var machineService: MachineService
+    @Autowired
+    private lateinit var aclManager: AclManager
 
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
     @DisplayName("자판기 등록")
     @Test
     fun register() {
+        //given
+        val address = AddressDto("12345", "삼성로", null)
+        val location = LocationDto(37.508855, 127.059479)
+        val managerIds = listOf(UUID.randomUUID(), UUID.randomUUID())
+        val managementGroupCodes = setOf("GROUP_UNIVERSITY_1", "GROUP_UNIVERSITY_2")
+
+        //when
+        val id = machineService.register(MACHINE_NO, MACHINE_NAME, VENDING, address, location, 100, managerIds.toSet(), managementGroupCodes)
+        clear()
+
+        //then
+        val findMachine = machineRepository.findByIdOrNull(id)!!
+        val findPrincipals = aclManager.getHasPermissionPrincipals(findMachine, BasePermission.READ)
+        val findAuthorities = aclManager.getHasPermissionAuthorities(findMachine, BasePermission.READ)
+
+        assertThat(findMachine.no).isEqualTo(MACHINE_NO)
+        assertThat(findMachine.name).isEqualTo(MACHINE_NAME)
+        assertThat(findMachine.type).isEqualTo(VENDING)
+        assertThat(findMachine.address.toDto()).isEqualTo(address)
+        assertThat(findMachine.location.toDto()).isEqualTo(location)
+        assertThat(findMachine.capacity).isEqualTo(100)
+        assertThat(findMachine.cupAmounts).isEqualTo(0)
+        assertThat(findMachine.state).isEqualTo(NORMAL)
+
+        assertThat(findPrincipals)
+            .containsExactlyInAnyOrder(managerIds[0], managerIds[1])
+
+        assertThat(findAuthorities)
+            .containsExactlyInAnyOrder(Role.ADMIN.key, Role.SYSTEM.key, "GROUP_UNIVERSITY_1", "GROUP_UNIVERSITY_2")
+    }
+
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @DisplayName("자판기 등록 TC2")
+    @Test
+    fun registerTc2() {
         //given
         val address = AddressDto("12345", "삼성로", null)
         val location = LocationDto(37.508855, 127.059479)
@@ -39,17 +82,26 @@ class MachineServiceTest : BaseServiceTest() {
         clear()
 
         //then
-        val machine = machineRepository.findByIdOrNull(id)
-        assertThat(machine?.no).isEqualTo(MACHINE_NO)
-        assertThat(machine?.name).isEqualTo(MACHINE_NAME)
-        assertThat(machine?.type).isEqualTo(VENDING)
-        assertThat(machine?.address?.toDto()).isEqualTo(address)
-        assertThat(machine?.location?.toDto()).isEqualTo(location)
-        assertThat(machine?.capacity).isEqualTo(100)
-        assertThat(machine?.cupAmounts).isEqualTo(0)
-        assertThat(machine?.state).isEqualTo(NORMAL)
+        val findMachine = machineRepository.findByIdOrNull(id)!!
+        val findPrincipals = aclManager.getHasPermissionPrincipals(findMachine, BasePermission.READ)
+        val findAuthorities = aclManager.getHasPermissionAuthorities(findMachine, BasePermission.READ)
+
+        assertThat(findMachine.no).isEqualTo(MACHINE_NO)
+        assertThat(findMachine.name).isEqualTo(MACHINE_NAME)
+        assertThat(findMachine.type).isEqualTo(VENDING)
+        assertThat(findMachine.address.toDto()).isEqualTo(address)
+        assertThat(findMachine.location.toDto()).isEqualTo(location)
+        assertThat(findMachine.capacity).isEqualTo(100)
+        assertThat(findMachine.cupAmounts).isEqualTo(0)
+        assertThat(findMachine.state).isEqualTo(NORMAL)
+
+        assertThat(findPrincipals).isEmpty()
+
+        assertThat(findAuthorities)
+            .containsExactlyInAnyOrder(Role.ADMIN.key, Role.SYSTEM.key)
     }
 
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
     @DisplayName("자판기 등록 예외")
     @Test
     fun registerException() {
@@ -97,7 +149,7 @@ class MachineServiceTest : BaseServiceTest() {
         clear()
 
         //when
-        val result = machineService.getAll(null, null, null, null, null, null, Pageable.unpaged())
+        val result = machineService.getAll(null, null, null, null, null, null, false, Pageable.unpaged())
 
         //then
         assertThat(result)
@@ -113,9 +165,9 @@ class MachineServiceTest : BaseServiceTest() {
         clear()
 
         //when
-        val result1 = machineService.getAll(null, null, null, null, "도산대로", null, Pageable.unpaged())
-        val result2 = machineService.getAll(null, null, COLLECTION, null, null, null, Pageable.unpaged())
-        val result3 = machineService.getAll(null, null, null, NORMAL, "삼성", null, Pageable.unpaged())
+        val result1 = machineService.getAll(null, null, null, null, null, "도산대로", false, Pageable.unpaged())
+        val result2 = machineService.getAll(null, null, null, COLLECTION, null, null, false, Pageable.unpaged())
+        val result3 = machineService.getAll(null, null, null, null, NORMAL, "삼성", false, Pageable.unpaged())
 
         //then
         assertThat(result1)
@@ -137,8 +189,8 @@ class MachineServiceTest : BaseServiceTest() {
         clear()
 
         //when
-        val result1 = machineService.getAll(null, null, null, null, "도산대로", null, Pageable.ofSize(1))
-        val result2 = machineService.getAll(null, null, null, null, null, null, Pageable.ofSize(3))
+        val result1 = machineService.getAll(null, null, null, null, null, "도산대로", null, Pageable.ofSize(1))
+        val result2 = machineService.getAll(null, null, null, null, null, null, null, Pageable.ofSize(3))
 
         //then
         assertThat(result1)
@@ -149,35 +201,43 @@ class MachineServiceTest : BaseServiceTest() {
             .containsExactly("0000001", "0000002", "0000003")
     }
 
-    @DisplayName("자판기 목록 조회 - 페이징")
+    @DisplayName("Simple 자판기 목록 조회 - 논페이징")
     @Test
-    fun getAllNotUsePaging() {
+    fun getAllSimple() {
         //given
         insertMachines()
         clear()
 
-        val result = machineService.getAll()
+        val result = machineService.getAllSimple(state = NORMAL, deleted = false)
 
         assertThat(result)
             .extracting("type")
-            .containsExactly(VENDING, VENDING, VENDING, VENDING, COLLECTION, VENDING)
+            .containsExactly(VENDING, VENDING, VENDING, VENDING, COLLECTION)
         assertThat(result)
             .extracting("location.latitude")
-            .containsExactly(37.508855, 37.508955, 37.508355, 37.508455, 37.518855, 37.503855)
+            .containsExactly(37.508855, 37.508955, 37.508355, 37.508455, 37.518855)
         assertThat(result)
             .extracting("location.longitude")
-            .containsExactly(127.059479, 127.052479, 127.051479, 127.053479, 127.029479, 127.059179)
+            .containsExactly(127.059479, 127.052479, 127.051479, 127.053479, 127.029479)
     }
 
     fun insertMachines() {
-        machineRepository.save(Machine("0000001", MACHINE_NAME, VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
-        machineRepository.save(Machine("0000002", MACHINE_NAME, VENDING, Address("00002", "삼성로", null), Location(37.508955, 127.052479), 100))
-        machineRepository.save(Machine("0000003", MACHINE_NAME, VENDING, Address("00003", "삼성로", null), Location(37.508355, 127.051479), 100))
-        machineRepository.save(Machine("0000004", MACHINE_NAME, VENDING, Address("00004", "마장로", null), Location(37.508455, 127.053479), 100))
-        machineRepository.save(Machine("0000005", MACHINE_NAME, COLLECTION, Address("00005", "도산대로", null), Location(37.518855, 127.029479), 100))
-        machineRepository.save(Machine("0000006", MACHINE_NAME, VENDING, Address("00006", "도산대로", null), Location(37.503855, 127.059179), 100))
+        val machine1 = machineRepository.save(Machine("0000001", MACHINE_NAME, VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+        val machine2 = machineRepository.save(Machine("0000002", MACHINE_NAME, VENDING, Address("00002", "삼성로", null), Location(37.508955, 127.052479), 100))
+        val machine3 = machineRepository.save(Machine("0000003", MACHINE_NAME, VENDING, Address("00003", "삼성로", null), Location(37.508355, 127.051479), 100))
+        val machine4 = machineRepository.save(Machine("0000004", MACHINE_NAME, VENDING, Address("00004", "마장로", null), Location(37.508455, 127.053479), 100))
+        val machine5 = machineRepository.save(Machine("0000005", MACHINE_NAME, COLLECTION, Address("00005", "도산대로", null), Location(37.518855, 127.029479), 100))
+        val machine6 = Machine("0000006", MACHINE_NAME, VENDING, Address("00006", "도산대로", null), Location(37.503855, 127.059179), 100)
+        val machine7 = Machine("0000007", MACHINE_NAME, VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100)
+
+        machine6.update(MACHINE_NAME, machine6.address, machine6.location, machine6.capacity, machine6.cupAmounts, MALFUNCTION)
+        machineRepository.save(machine6)
+
+        machine7.delete()
+        machineRepository.save(machine7)
     }
 
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
     @DisplayName("자판기 정보 수정")
     @Test
     fun update() {
@@ -208,6 +268,53 @@ class MachineServiceTest : BaseServiceTest() {
         assertThat(findMachine?.state).isEqualTo(MALFUNCTION)
     }
 
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @DisplayName("자판기 정보 수정 TC2")
+    @Test
+    fun updateTc2() {
+        //given
+        val address = Address("12345", "삼성로", null)
+        val location = Location(37.508855, 127.059479)
+        val machine = machineRepository.save(Machine(MACHINE_NO, MACHINE_NAME, VENDING, address, location, 100))
+        val managerIds = listOf(UUID.randomUUID(), UUID.randomUUID())
+        val managementGroupCodes = setOf("GROUP_UNIVERSITY_1", "GROUP_UNIVERSITY_2")
+        aclManager.addPermissionForPrincipals(machine, BasePermission.READ, *managerIds.toTypedArray())
+        aclManager.addPermissionForAuthorities(machine, BasePermission.READ, *managementGroupCodes.toTypedArray())
+        clear()
+
+        //when
+        val newAddress = AddressDto("00000", "마장로", null)
+        val newLocation = LocationDto(37.508855, 127.029479)
+        val newManagerIds = setOf(UUID.randomUUID())
+        val newManagementGroupCodes = setOf("GROUP_UNIVERSITY_2", "GROUP_UNIVERSITY_3")
+        machineService.update(machine.id, "new name", newAddress, newLocation, 200, 10, MALFUNCTION, newManagerIds, newManagementGroupCodes)
+        clear()
+
+        //then
+        val findMachine = machineRepository.findByIdOrNull(machine.id)!!
+        val findPrincipals = aclManager.getHasPermissionPrincipals(findMachine, BasePermission.READ)
+        val findAuthorities = aclManager.getHasPermissionAuthorities(findMachine, BasePermission.READ)
+
+        assertThat(findMachine.no).isEqualTo(MACHINE_NO)
+        assertThat(findMachine.name).isEqualTo("new name")
+        assertThat(findMachine.type).isEqualTo(VENDING)
+        assertThat(findMachine.address.zipCode).isEqualTo("00000")
+        assertThat(findMachine.address.address1).isEqualTo("마장로")
+        assertThat(findMachine.address.address2).isNull()
+        assertThat(findMachine.location.latitude).isEqualTo(37.508855)
+        assertThat(findMachine.location.longitude).isEqualTo(127.029479)
+        assertThat(findMachine.capacity).isEqualTo(200)
+        assertThat(findMachine.cupAmounts).isEqualTo(10)
+        assertThat(findMachine.state).isEqualTo(MALFUNCTION)
+
+        assertThat(findPrincipals)
+            .containsExactlyInAnyOrder(*newManagerIds.toTypedArray())
+
+        assertThat(findAuthorities)
+            .containsExactlyInAnyOrder("GROUP_UNIVERSITY_2", "GROUP_UNIVERSITY_3")
+    }
+
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
     @DisplayName("자판기 삭제")
     @Test
     fun delete() {
@@ -225,5 +332,31 @@ class MachineServiceTest : BaseServiceTest() {
         val findMachine = machineRepository.findByIdOrNull(machine.id)
         assertThat(findMachine?.no).startsWith("Deleted")
         assertThat(findMachine?.deleted).isTrue()
+    }
+
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @DisplayName("자판기 삭제 TC2")
+    @Test
+    fun deleteTc2() {
+        //given
+        val address = Address("12345", "삼성로", null)
+        val location = Location(37.508855, 127.059479)
+        val managementGroupCodes = setOf("GROUP_UNIVERSITY_1", "GROUP_UNIVERSITY_2")
+        val machine = machineRepository.save(Machine(MACHINE_NO, MACHINE_NAME, VENDING, address, location, 100))
+        aclManager.addPermissionForAuthorities(machine, BasePermission.READ, *managementGroupCodes.toTypedArray())
+        clear()
+
+        //when
+        machineService.delete(machine.id)
+        clear()
+
+        //then
+        val findMachine = machineRepository.findByIdOrNull(machine.id)!!
+        val findAuthorities = aclManager.getHasPermissionAuthorities(findMachine, BasePermission.READ)
+
+        assertThat(findMachine.no).startsWith("Deleted")
+        assertThat(findMachine.deleted).isTrue()
+
+        assertThat(findAuthorities).isEmpty()
     }
 }
--- src/test/kotlin/com/liah/doribottle/web/admin/machine/MachineResourceTest.kt
+++ src/test/kotlin/com/liah/doribottle/web/admin/machine/MachineResourceTest.kt
@@ -2,13 +2,13 @@ package com.liah.doribottle.web.admin.machine
 
 import com.liah.doribottle.common.error.exception.ErrorCode
 import com.liah.doribottle.config.security.WithMockDoriUser
+import com.liah.doribottle.config.security.acl.AclManager
 import com.liah.doribottle.domain.common.Address
 import com.liah.doribottle.domain.common.Location
 import com.liah.doribottle.domain.machine.Machine
 import com.liah.doribottle.domain.machine.MachineState.NORMAL
 import com.liah.doribottle.domain.machine.MachineType.COLLECTION
 import com.liah.doribottle.domain.machine.MachineType.VENDING
-import com.liah.doribottle.domain.user.Admin
 import com.liah.doribottle.domain.user.Role
 import com.liah.doribottle.extension.convertAnyToString
 import com.liah.doribottle.repository.machine.MachineRepository
@@ -24,9 +24,11 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.security.acls.domain.BasePermission
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 
@@ -35,6 +37,8 @@ class MachineResourceTest : BaseControllerTest() {
 
     @Autowired
     private lateinit var machineRepository: MachineRepository
+    @Autowired
+    private lateinit var aclManager: AclManager
 
     @AfterEach
     internal fun destroy() {
@@ -91,9 +95,11 @@ class MachineResourceTest : BaseControllerTest() {
 
     @DisplayName("자판기 조회")
     @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @Transactional
     @Test
     fun get() {
         val machine = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+        aclManager.addPermissionForAuthorities(machine, BasePermission.READ, Role.ADMIN.key)
 
         mockMvc.perform(
             get("${endPoint}/${machine.id}")
@@ -114,8 +120,23 @@ class MachineResourceTest : BaseControllerTest() {
             .andExpect(jsonPath("state", `is`(machine.state.name)))
     }
 
+    @DisplayName("자판기 조회 - 예외")
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @Test
+    fun getException() {
+        val machine = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+
+        mockMvc.perform(
+            get("${endPoint}/${machine.id}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isForbidden)
+    }
+
     @DisplayName("자판기 목록 조회")
     @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @Transactional
     @Test
     fun getAll() {
         insertMachines()
@@ -125,7 +146,30 @@ class MachineResourceTest : BaseControllerTest() {
         params.add("page", "0")
         params.add("size", "3")
 
-        val expectValue = listOf("0000006", "0000004", "0000003")
+        val expectValue = listOf("0000004", "0000003", "0000002")
+        mockMvc.perform(
+            get(endPoint)
+                .params(params)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("content[*].no", `is`(expectValue)))
+    }
+
+    @DisplayName("자판기 목록 조회 TC2")
+    @WithMockDoriUser(loginId = MACHINE_LOGIN_ID, role = Role.MACHINE_ADMIN)
+    @Transactional
+    @Test
+    fun getAllTc2() {
+        insertMachines()
+
+        val params: MultiValueMap<String, String> = LinkedMultiValueMap()
+        params.add("type", "VENDING")
+        params.add("page", "0")
+        params.add("size", "3")
+
+        val expectValue = listOf("0000006")
         mockMvc.perform(
             get(endPoint)
                 .params(params)
@@ -137,19 +181,29 @@ class MachineResourceTest : BaseControllerTest() {
     }
 
     private fun insertMachines() {
-        machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479),  100))
-        machineRepository.save(Machine("0000002", "name", VENDING, Address("00002", "삼성로", null), Location(37.508855, 127.059479),  100))
-        machineRepository.save(Machine("0000003", "name", VENDING, Address("00003", "삼성로", null), Location(37.508855, 127.059479),  100))
-        machineRepository.save(Machine("0000004", "name", VENDING, Address("00004", "마장로", null), Location(37.508855, 127.059479),  100))
-        machineRepository.save(Machine("0000005", "name", COLLECTION, Address("00005", "도산대로", null), Location(37.508855, 127.059479),  100))
-        machineRepository.save(Machine("0000006", "name", VENDING, Address("00006", "도산대로", null), Location(37.508855, 127.059479),  100))
+        val machine1 = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479),  100))
+        val machine2 = machineRepository.save(Machine("0000002", "name", VENDING, Address("00002", "삼성로", null), Location(37.508855, 127.059479),  100))
+        val machine3 = machineRepository.save(Machine("0000003", "name", VENDING, Address("00003", "삼성로", null), Location(37.508855, 127.059479),  100))
+        val machine4 = machineRepository.save(Machine("0000004", "name", VENDING, Address("00004", "마장로", null), Location(37.508855, 127.059479),  100))
+        val machine5 = machineRepository.save(Machine("0000005", "name", COLLECTION, Address("00005", "도산대로", null), Location(37.508855, 127.059479),  100))
+        val machine6 = machineRepository.save(Machine("0000006", "name", VENDING, Address("00006", "도산대로", null), Location(37.508855, 127.059479),  100))
+
+        aclManager.addPermissionForAuthorities(machine1, BasePermission.READ, Role.ADMIN.key)
+        aclManager.addPermissionForAuthorities(machine2, BasePermission.READ, Role.ADMIN.key)
+        aclManager.addPermissionForAuthorities(machine3, BasePermission.READ, Role.ADMIN.key)
+        aclManager.addPermissionForAuthorities(machine4, BasePermission.READ, Role.ADMIN.key)
+        aclManager.addPermissionForAuthorities(machine5, BasePermission.READ, Role.MACHINE_ADMIN.key)
+        aclManager.addPermissionForAuthorities(machine6, BasePermission.READ, Role.MACHINE_ADMIN.key)
     }
 
     @DisplayName("자판기 정보 수정")
     @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @Transactional
     @Test
     fun update() {
         val machine = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+        aclManager.addPermissionForAuthorities(machine, BasePermission.WRITE, Role.ADMIN.key)
+
         val body = MachineUpdateRequest("name", AddressDto("12345", "삼성로"), LocationDto(37.508855, 127.059479), 100, 50, NORMAL)
 
         mockMvc.perform(
@@ -166,6 +220,7 @@ class MachineResourceTest : BaseControllerTest() {
     @Test
     fun updateExceptionTc2() {
         val machine = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+
         val body = MachineUpdateRequest("name", AddressDto("12345", "삼성로"), LocationDto(37.508855, 127.059479), 100, -1, NORMAL)
 
         mockMvc.perform(
@@ -178,11 +233,32 @@ class MachineResourceTest : BaseControllerTest() {
             .andExpect(jsonPath("code", `is`(ErrorCode.INVALID_INPUT_VALUE.code)))
     }
 
+    @DisplayName("자판기 정보 수정 - 예외 TC3")
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @Test
+    fun updateExceptionTc3() {
+        val machine = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+
+        val body = MachineUpdateRequest("name", AddressDto("12345", "삼성로"), LocationDto(37.508855, 127.059479), 100, 50, NORMAL)
+
+        mockMvc.perform(
+            put("$endPoint/${machine.id}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(body.convertAnyToString())
+        )
+            .andExpect(status().isForbidden)
+            .andExpect(jsonPath("code", `is`(ErrorCode.ACCESS_DENIED.code)))
+    }
+
     @DisplayName("자판기 정보 패치")
     @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @Transactional
     @Test
     fun patch() {
         val machine = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+        aclManager.addPermissionForAuthorities(machine, BasePermission.WRITE, Role.ADMIN.key)
+
         val body = MachinePatchRequest("updated",  null, null, null, 10, null)
 
         mockMvc.perform(
@@ -196,9 +272,12 @@ class MachineResourceTest : BaseControllerTest() {
 
     @DisplayName("자판기 정보 패치 POST")
     @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @Transactional
     @Test
     fun patchPost() {
         val machine = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+        aclManager.addPermissionForAuthorities(machine, BasePermission.WRITE, Role.ADMIN.key)
+
         val body = MachinePatchRequest("updated",  null, null, null, 10, null)
 
         mockMvc.perform(
@@ -212,9 +291,11 @@ class MachineResourceTest : BaseControllerTest() {
 
     @DisplayName("기기 삭제")
     @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @Transactional
     @Test
     fun delete() {
         val machine = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+        aclManager.addPermissionForAuthorities(machine, BasePermission.DELETE, Role.ADMIN.key)
 
         mockMvc.perform(
             delete("$endPoint/${machine.id}")
@@ -222,5 +303,20 @@ class MachineResourceTest : BaseControllerTest() {
                 .accept(MediaType.APPLICATION_JSON)
         )
             .andExpect(status().isOk)
+    }
+
+    @DisplayName("기기 삭제 - 예외 TC2")
+    @WithMockDoriUser(loginId = ADMIN_LOGIN_ID, role = Role.ADMIN)
+    @Test
+    fun deleteExceptionTc2() {
+        val machine = machineRepository.save(Machine("0000001", "name", VENDING, Address("00001", "삼성로", null), Location(37.508855, 127.059479), 100))
+
+        mockMvc.perform(
+            delete("$endPoint/${machine.id}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isForbidden)
+            .andExpect(jsonPath("code", `is`(ErrorCode.ACCESS_DENIED.code)))
     }
 }
