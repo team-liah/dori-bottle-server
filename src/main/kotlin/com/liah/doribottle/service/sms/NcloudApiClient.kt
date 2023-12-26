6642cfdd3d98f3dece47bb83acfb487f64faa3c8
doongjun <doongjun@kakao.com>
2023-12-26 18:58:31 +0900

    feat: ncloud

---
+++ src/main/kotlin/com/liah/doribottle/service/sms/NcloudApiClient.kt
@@ -0,0 +1,10 @@
+package com.liah.doribottle.service.sms
+
+import com.liah.doribottle.config.properties.AppProperties
+import org.springframework.stereotype.Component
+
+@Component
+class NcloudApiClient(
+    appProperties: AppProperties
+) {
+}
