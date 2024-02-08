71c09beba64c3398e2ed9ef2e73ace94be24a897
doongjun <doongjun@kakao.com>
2024-02-08 14:57:49 +0900

    refactor: generateRandomString

--- src/main/kotlin/com/liah/doribottle/extension/StringExtension.kt
+++ src/main/kotlin/com/liah/doribottle/extension/StringExtension.kt
@@ -2,6 +2,7 @@ package com.liah.doribottle.extension
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.util.*
 import java.util.concurrent.ThreadLocalRandom
 
 fun Any.convertAnyToString(): String {
@@ -13,7 +14,9 @@ fun <T> String.convertStringToAny(): T {
     return  jacksonObjectMapper().readValue(this, typeReference)
 }
 
-private val charPool = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-fun generateRandomString(size: Int) = List(size) { charPool.random() }.joinToString("")
+fun generateRandomString(size: Int): String {
+    val randomString = Base64.getUrlEncoder().encodeToString(UUID.randomUUID().toString().toByteArray())
+    return randomString.substring(0 until size)
+}
 
 fun generateRandomNumberString() = String.format("%06d", ThreadLocalRandom.current().nextInt(999999))
