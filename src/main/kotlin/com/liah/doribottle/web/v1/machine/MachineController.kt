18326e64a97788d4bdd7727bab8308bbb8efe10e
doongjun <doongjun@kakao.com>
2024-02-16 13:28:58 +0900

    refactor: Machine getAll

--- src/main/kotlin/com/liah/doribottle/web/v1/machine/MachineController.kt
+++ src/main/kotlin/com/liah/doribottle/web/v1/machine/MachineController.kt
@@ -4,6 +4,7 @@ import com.liah.doribottle.service.machine.MachineService
 import com.liah.doribottle.web.v1.machine.vm.MachineResponse
 import com.liah.doribottle.web.v1.machine.vm.MachineSimpleResponse
 import io.swagger.v3.oas.annotations.Operation
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,8 +19,9 @@ class MachineController(
     @Operation(summary = "기기 전체 조회")
     @GetMapping("/all")
     fun getAll(): List<MachineSimpleResponse> {
-        return machineService.getAll()
-            .map { it.toResponse() }
+        return machineService.getAllSimple(
+            pageable = Pageable.unpaged()
+        ).content.map { it.toResponse() }
     }
 
     @Operation(summary = "기기 조회")
