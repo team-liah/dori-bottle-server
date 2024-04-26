098805316f11c4aa8d3e46bcc585a9e2e9e3c4e5
doongjun <doongjun.kim@gmail.com>
2024-04-26 14:18:32 +0900

    fix: Build failed

--- src/test/kotlin/com/liah/doribottle/DoriBottleApplicationTests.kt
+++ src/test/kotlin/com/liah/doribottle/DoriBottleApplicationTests.kt
@@ -1,15 +1,16 @@
 package com.liah.doribottle
 
+import com.liah.doribottle.config.TestcontainersConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
+@Import(TestcontainersConfig::class)
 @SpringBootTest
 class DoriBottleApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
